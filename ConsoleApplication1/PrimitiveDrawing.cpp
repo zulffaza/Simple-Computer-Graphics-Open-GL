83873f303ea5a410e6584d271299bacac97f947f
Faza Zulfika P P <zulffaza@gmail.com>
2017-11-20 13:38:20 +0700

    Update sampai obyek 3D

--- ConsoleApplication1/PrimitiveDrawing.cpp
+++ ConsoleApplication1/PrimitiveDrawing.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "stdafx.h" // Header yang dibuat oleh programmer
+#include <stdlib.h>
 #include <glut.h> // Header yang tidak dibuat oleh programmer
 #include <math.h>
 
@@ -21,6 +22,50 @@ typedef struct point2D_d {
 	double y;
 };
 
+typedef struct point3D_i {
+	int x;
+	int y;
+	int z;
+};
+
+typedef struct point3D_f {
+	float x;
+	float y;
+	float z;
+};
+
+typedef struct point3D_d {
+	double x;
+	double y;
+	double z;
+};
+
+typedef struct face_t {
+	int numberOfVertices;
+	short int point[32];
+};
+
+typedef struct object3D_i {
+	int numberOfVertices;
+	point3D_i point[100];
+	int numberOfFaces;
+	face_t face[32];
+};
+
+typedef struct object3D_f {
+	int numberOfVertices;
+	point3D_f point[100];
+	int numberOfFaces;
+	face_t face[32];
+};
+
+typedef struct object3D_d {
+	int numberOfVertices;
+	point3D_d point[100];
+	int numberOfFaces;
+	face_t face[32];
+};
+
 typedef struct vector2D_i
 {
 	int v[3];
@@ -34,6 +79,21 @@ typedef struct vector2D_f
 typedef struct vector2D_d
 {
 	double v[3];
+};
+
+typedef struct vector3D_i
+{
+	int v[4];
+};
+
+typedef struct vector3D_f
+{
+	float v[4];
+};
+
+typedef struct vector3D_d
+{
+	double v[4];
 };
 
 typedef struct matrix2D_i
@@ -120,6 +180,39 @@ vector2D_d point2D_d_to_vector2D_d(point2D_d point) {
 	return vector;
 }
 
+vector3D_i point3D_i_to_vector3D_i(point3D_i point) {
+	vector3D_i vector;
+
+	vector.v[0] = point.x;
+	vector.v[1] = point.y;
+	vector.v[2] = point.z;
+	vector.v[3] = 1;
+
+	return vector;
+}
+
+vector3D_f point3D_f_to_vector3D_f(point3D_f point) {
+	vector3D_f vector;
+
+	vector.v[0] = point.x;
+	vector.v[1] = point.y;
+	vector.v[2] = point.z;
+	vector.v[3] = 1;
+
+	return vector;
+}
+
+vector3D_d point3D_d_to_vector3D_d(point3D_d point) {
+	vector3D_d vector;
+
+	vector.v[0] = point.x;
+	vector.v[1] = point.y;
+	vector.v[2] = point.z;
+	vector.v[3] = 1;
+
+	return vector;
+}
+
 point2D_i vector2D_i_to_point2D_i(vector2D_i vector) {
 	point2D_i point;
 
@@ -139,6 +232,33 @@ point2D_f vector2D_f_to_point2D_f(vector2D_f vector) {
 }
 
 point2D_d vector2D_d_to_point2D_d(vector2D_d vector) {
+	point2D_d point;
+
+	point.x = vector.v[0];
+	point.y = vector.v[1];
+
+	return point;
+}
+
+point2D_i vector3D_i_to_point2D_i(vector3D_i vector) {
+	point2D_i point;
+
+	point.x = vector.v[0];
+	point.y = vector.v[1];
+
+	return point;
+}
+
+point2D_f vector3D_f_to_point2D_f(vector3D_f vector) {
+	point2D_f point;
+
+	point.x = vector.v[0];
+	point.y = vector.v[1];
+
+	return point;
+}
+
+point2D_d vector3D_d_to_point2D_d(vector3D_d vector) {
 	point2D_d point;
 
 	point.x = vector.v[0];
@@ -1311,12 +1431,12 @@ void drawFillPolygon(point2D_f point[], int n, colorRGB_f color) {
 	glEnd();
 }
 
-void drawGradatePolygon(point2D_f point[], int n, colorRGB_f color) {
+void drawGradatePolygon(point2D_f point[], int n, colorRGB_f color[]) {
 	int i;
 
 	glBegin(GL_POLYGON);
 		for (i = 0; i < n; i++) {
-			glColor3f(color.r, color.g, color.b);
+			glColor3f(color[i].r, color[i].g, color[i].b);
 			glVertex2f(point[i].x, point[i].y);
 		}
 	glEnd();
@@ -1575,8 +1695,69 @@ matrix2D_d scalingMatrix(double dx, double dy) {
 matrix2D_f rotationMatrix(float tetha) {
 	matrix2D_f mF = createFloatIdentity();
 
-	float cs = cos(tetha);
-	float sn = sin(tetha);
+	float val = 3.14159 / 180;
+	float cs = cos(tetha * val);
+	float sn = sin(tetha * val);
+
+	mF.m[0][0] = cs;
+	mF.m[1][0] = sn;
+	mF.m[0][1] = -sn;
+	mF.m[1][1] = cs;
+
+	return mF;
+}
+
+matrix2D_f rotationMatrixX(float tetha) {
+	matrix2D_f mF = createFloatIdentity();
+
+	float val = 3.14159 / 180;
+	float cs = cos(tetha * val);
+	float sn = sin(tetha * val);
+
+	mF.m[1][1] = cs;
+	mF.m[2][1] = sn;
+	mF.m[1][2] = -sn;
+	mF.m[2][2] = cs;
+
+	return mF;
+}
+
+matrix2D_f rotationMatrixY(float tetha) {
+	matrix2D_f mF = createFloatIdentity();
+
+	float val = 3.14159 / 180;
+	float cs = cos(tetha * val);
+	float sn = sin(tetha * val);
+
+	mF.m[0][0] = cs;
+	mF.m[0][2] = sn;
+	mF.m[2][0] = -sn;
+	mF.m[2][2] = cs;
+
+	return mF;
+}
+
+matrix2D_f rotationMatrixZ(float tetha) {
+	matrix2D_f mF = createFloatIdentity();
+
+	float val = 3.14159 / 180;
+	float cs = cos(tetha * val);
+	float sn = sin(tetha * val);
+
+	mF.m[0][0] = cs;
+	mF.m[1][0] = sn;
+	mF.m[0][1] = -sn;
+	mF.m[1][1] = cs;
+
+	return mF;
+}
+
+matrix2D_f rotationMatrix(float tetha, point2D_f point) {
+	matrix2D_f mF = createFloatIdentity();
+
+	float val = 3.14159 / 180;
+	float cs = point.x + (tetha * val);
+	float sn = point.y + (tetha * val);
 
 	mF.m[0][0] = cs;
 	mF.m[1][0] = sn;
@@ -1589,8 +1770,24 @@ matrix2D_f rotationMatrix(float tetha) {
 matrix2D_d rotationMatrix(double tetha) {
 	matrix2D_d mD = createDoubleIdentity();
 
-	double cs = cos(tetha);
-	double sn = sin(tetha);
+	float val = 3.14159 / 180;
+	float cs = cos(tetha * val);
+	float sn = sin(tetha * val);
+
+	mD.m[0][0] = cs;
+	mD.m[1][0] = sn;
+	mD.m[0][1] = -sn;
+	mD.m[1][1] = cs;
+
+	return mD;
+}
+
+matrix2D_d rotationMatrix(double tetha, point2D_d point) {
+	matrix2D_d mD = createDoubleIdentity();
+
+	float val = 3.14159 / 180;
+	float cs = point.x + (tetha * val);
+	float sn = point.y + (tetha * val);
 
 	mD.m[0][0] = cs;
 	mD.m[1][0] = sn;
@@ -1690,22 +1887,110 @@ vector2D_d matrixXvector(matrix2D_d m, vector2D_d v) {
 	return vX;
 }
 
-void draw() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(0, 0, 0); // Set color of draw
+vector3D_i vectorMinVector3D(vector3D_i a, vector3D_i b) {
+	vector3D_i c;
 
+	c.v[0] = a.v[0] - b.v[0];
+	c.v[1] = a.v[1] - b.v[1];
+	c.v[2] = a.v[2] - b.v[2];
+	c.v[3] = 1;
 
-	//drawCoordinateLines_2color2f({ 0, 0, 0 }, { 0, 0, 0 });
-	//drawTrianglei_colorf({ -100, 100 }, { -200, 100 }, { -150, 200 }, { 0, 0, 1 });
-	//draw5PointsStari({ 100, 100 }, { 150, 200 }, { 200, 100 }, { 75, 160 }, { 225, 160 });
-	//drawRectanglei_colorf({ -100, -100 }, { -200, -200 }, { 1, 0, 1 });
-	//drawRectangle2i_colorf({ 30, -30 }, { 200, -30 }, { 200, -300 }, { 30, -300 }, { 0, 1, 1 });
+	return c;
+}
 
-	//point2D_i kotak[4] = {{ 100, 100 }, { 300, 100 }, { 300, 200 }, { 100, 200 }};
-	//drawPolygon(kotak, 4);
+vector3D_f vectorMinVector3D(vector3D_f a, vector3D_f b) {
+	vector3D_f c;
 
-	//point2D_i bintang[10] = {{ 80, 146 }, { 99, 90 }, { 157, 90 }, { 110, 55 }, { 128, 1 }, { 80, 34 }, { 32, 1 }, { 54, 55 }, { 3, 90 }, { 63, 90 }};
-	//drawPolygon(bintang, 10);
+	c.v[0] = a.v[0] - b.v[0];
+	c.v[1] = a.v[1] - b.v[1];
+	c.v[2] = a.v[2] - b.v[2];
+	c.v[3] = 1;
+
+	return c;
+}
+
+vector3D_d vectorMinVector3D(vector3D_d a, vector3D_d b) {
+	vector3D_d c;
+
+	c.v[0] = a.v[0] - b.v[0];
+	c.v[1] = a.v[1] - b.v[1];
+	c.v[2] = a.v[2] - b.v[2];
+	c.v[3] = 1;
+
+	return c;
+}
+
+vector3D_i vectorCrossVector3D(vector3D_i a, vector3D_i b) {
+	vector3D_i c;
+	
+	c.v[0] = a.v[1] * b.v[2] - a.v[2] * b.v[1];
+	c.v[1] = a.v[2] * b.v[0] - a.v[0] * b.v[2];
+	c.v[2] = a.v[0] * b.v[1] - a.v[1] * b.v[0];
+	c.v[3] = 1;
+	
+	return c;
+}
+
+vector3D_f vectorCrossVector3D(vector3D_f a, vector3D_f b) {
+	vector3D_f c;
+
+	c.v[0] = a.v[1] * b.v[2] - a.v[2] * b.v[1];
+	c.v[1] = a.v[2] * b.v[0] - a.v[0] * b.v[2];
+	c.v[2] = a.v[0] * b.v[1] - a.v[1] * b.v[0];
+	c.v[3] = 1;
+
+	return c;
+}
+
+vector3D_d vectorCrossVector3D(vector3D_d a, vector3D_d b) {
+	vector3D_d c;
+
+	c.v[0] = a.v[1] * b.v[2] - a.v[2] * b.v[1];
+	c.v[1] = a.v[2] * b.v[0] - a.v[0] * b.v[2];
+	c.v[2] = a.v[0] * b.v[1] - a.v[1] * b.v[0];
+	c.v[3] = 1;
+
+	return c;
+}
+
+void createGalaxy() {
+	static int star = 0;
+	static point2D_f starPoint[10];
+	static vector2D_f starVector[10];
+	static matrix2D_f starMatrix[10];
+
+	if (star == 0) {
+		starPoint[0] = { -80, -80 };
+		starPoint[1] = { -50, -10 };
+		starPoint[2] = { -125, 30 };
+		starPoint[3] = { -30, 30 };
+		starPoint[4] = { 0, 100 };
+		starPoint[5] = { 30, 30 };
+		starPoint[6] = { 125, 30 };
+		starPoint[7] = { 50, -10 };
+		starPoint[8] = { 80, -80 };
+		starPoint[9] = { 0, -30 };
+
+		for (int i = 0; i < 10; i++)
+			starVector[i] = point2D_f_to_vector2D_f(starPoint[i]);
+
+		star = 1;
+	}
+	else {
+		int i;
+
+		for (i = 0; i < 10; i++)
+			starMatrix[i] = rotationMatrix((float)1);
+
+		for (i = 0; i < 10; i++)
+			starVector[i] = matrixXvector(starMatrix[i], starVector[i]);
+
+		for (i = 0; i < 10; i++)
+			starPoint[i] = vector2D_f_to_point2D_f(starVector[i]);
+	}
+
+	//drawFillPolygon(starPoint, 10, { 1, 1, 0 });
+	drawPolygon(starPoint, 10);
 
 	//drawRose_colorf(1000, { 1, 0, 0 });
 	//drawRose_colorf(100, { 0, 0, 1 });
@@ -1718,53 +2003,308 @@ void draw() {
 	//drawSinMoveWithPoint();
 
 	static int earth = 0, moon = 0;
-	static point2D_f earthPoint, moonPoint;
+	static point2D_f earthPoint, moonPoint, newPoint;
 	static vector2D_f earthVector, moonVector;
 	static matrix2D_f earthMatrix, moonMatrix;
 
 	point2D_f sunOrbit[360], earthOrbit[360];
 	float n = 360;
 
-	drawCircle(60, n, { 1, 1, 0.5 });
+	//drawCircle(60, n, { 1, 1, 0.5 });
+	//draw5PointsStar({ -43, -43 }, { 0, 60 }, { 43, -43 }, { -60, 10 }, { 60, 10 });
 	glColor3f(1, 0.5, 0);
-	drawCircle(60, n);
+	//drawCircle(60, n);
 	glColor3f(0, 0, 0);
 	drawCircleAtPoint(sunOrbit, { 0, 0 }, 200, n);
 
-	/*
 	if (earth == 0) {
 		earthPoint = sunOrbit[earth];
 		earthVector = point2D_f_to_vector2D_f(earthPoint);
-	} else {
-		earthMatrix = rotationMatrix((float) earth);
+
+		earth = 1;
+	}
+	else {
+		earthMatrix = rotationMatrix((float)1);
 		earthVector = matrixXvector(earthMatrix, earthVector);
 		earthPoint = vector2D_f_to_point2D_f(earthVector);
 	}
+
+	drawCircleAtPoint(earthPoint, 20, n, { 0, 0.75, 0 });
+	glColor3f(0, 0, 0);
+	drawCircleAtPoint(earthPoint, 20, n);
+	drawCircleAtPoint(earthOrbit, earthPoint, 45, n);
+
+	if (moon == 0) {
+		moonPoint = earthOrbit[moon];
+		moonVector = point2D_f_to_vector2D_f(moonPoint);
+
+		moon = 1;
+	}
+	else {
+		moonMatrix = rotationMatrix((float)1);
+		moonVector = matrixXvector(moonMatrix, moonVector);
+		moonPoint = vector2D_f_to_point2D_f(moonVector);
+
+		moonPoint.x -= earthPoint.x;
+		moonPoint.y -= earthPoint.y;
+
+		moonVector = point2D_f_to_vector2D_f(moonPoint);
+
+		moonMatrix = rotationMatrix((float)4);
+		moonVector = matrixXvector(moonMatrix, moonVector);
+		moonPoint = vector2D_f_to_point2D_f(moonVector);
+
+		moonPoint.x += earthPoint.x;
+		moonPoint.y += earthPoint.y;
+	}
+
+	drawCircleAtPoint(moonPoint, 10, n, { 1, 0.5, 0 });
+	glColor3f(0, 0, 0);
+	drawCircleAtPoint(moonPoint, 10, n);
+}
+
+void createGlassWithCover() {
+	point2D_f point1[4];
+
+	point1[0] = { -100, 0 };
+	point1[1] = { 0, 0 };
+	point1[2] = { 0, -150 };
+	point1[3] = { -100, -150 };
+
+	colorRGB_f color1[4];
+
+	color1[0] = { 0, 1, 0 };
+	color1[1] = { 0, 1, 0.5 };
+	color1[2] = { 0, 0.5, 1 };
+	color1[3] = { 0, 0, 1 };
+
+	drawGradatePolygon(point1, 4, color1);
+
+	static int cover = 0;
+	static float angle = -1;
+	static point2D_f point2[4], point3[4], point4[4], point5[4];
+	static vector2D_f vector2[4], vector3[4], vector4[4], vector5[4];
+	static matrix2D_f matrix2[4], matrix3[4], matrix4[4], matrix5[4];
+
+	if (cover == 0) {
+		point2[3] = { -100, 20 };
+		point2[2] = { 0, 20 };
+		point2[1] = { 0, 0 };
+		point2[0] = { -100, 0 };
+
+		point3[0] = { -75, 40 };
+		point3[1] = { -25, 40 };
+		point3[2] = { -25, 20 };
+		point3[3] = { -75, 20 };
+
+		point4[3] = { -100, -150 };
+		point4[2] = { 0, -150 };
+		point4[1] = { 0, -170 };
+		point4[0] = { -100, -170 };
+
+		point5[3] = { -75, -190 };
+		point5[2] = { -25, -190 };
+		point5[1] = { -25, -170 };
+		point5[0] = { -75, -170 };
+
+		for (int i = 0; i < 4; i++) {
+			vector2[i] = point2D_f_to_vector2D_f(point2[i]);
+			vector3[i] = point2D_f_to_vector2D_f(point3[i]);
+			vector4[i] = point2D_f_to_vector2D_f(point4[i]);
+			vector5[i] = point2D_f_to_vector2D_f(point5[i]);
+		}
+
+		cover = 1;
+	}
+	else {
+		int i;
+
+		for (i = 0; i < 4; i++) {
+			if (i != 2) {
+				point4[i].x -= point4[2].x;
+				point4[i].y -= point4[2].y;
+			}
+
+			point5[i].x -= point4[2].x;
+			point5[i].y -= point4[2].y;
+
+			vector4[i] = point2D_f_to_vector2D_f(point4[i]);
+			vector5[i] = point2D_f_to_vector2D_f(point5[i]);
+		}
+
+
+		for (i = 0; i < 4; i++) {
+			matrix2[i] = rotationMatrix(angle);
+			matrix3[i] = rotationMatrix(angle);
+			matrix4[i] = rotationMatrix(-angle);
+			matrix5[i] = rotationMatrix(-angle);
+		}
+
+		for (i = 0; i < 4; i++) {
+			if (i != 1)
+				vector2[i] = matrixXvector(matrix2[i], vector2[i]);
+
+			if (i != 2)
+				vector4[i] = matrixXvector(matrix4[i], vector4[i]);
+
+			vector3[i] = matrixXvector(matrix3[i], vector3[i]);
+			vector5[i] = matrixXvector(matrix5[i], vector5[i]);
+		}
+
+		for (i = 0; i < 4; i++) {
+			if (i != 1)
+				point2[i] = vector2D_f_to_point2D_f(vector2[i]);
+
+			if (i != 2)
+				point4[i] = vector2D_f_to_point2D_f(vector4[i]);
+
+			point3[i] = vector2D_f_to_point2D_f(vector3[i]);
+			point5[i] = vector2D_f_to_point2D_f(vector5[i]);
+		}
+
+		for (i = 0; i < 4; i++) {
+			if (i != 2) {
+				point4[i].x += point4[2].x;
+				point4[i].y += point4[2].y;
+			}
+
+			point5[i].x += point4[2].x;
+			point5[i].y += point4[2].y;
+		}
+
+		if ((int)point2[0].x == 0)
+			angle = 1;
+
+		if ((int)point2[0].y == 0)
+			angle = -1;
+	}
+
+	drawGradatePolygon(point2, 4, color1);
+	drawGradatePolygon(point3, 4, color1);
+	drawGradatePolygon(point4, 4, color1);
+	drawGradatePolygon(point5, 4, color1);
+}
+
+void draw() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0, 0, 0); // Set color of draw
+
+	colorRGB_f color = { 1, 0, 0 };
+
+	//drawCoordinateLines(color, color);
+	//drawTrianglei_colorf({ -100, 100 }, { -200, 100 }, { -150, 200 }, { 0, 0, 1 });
+	//draw5PointsStari({ 100, 100 }, { 150, 200 }, { 200, 100 }, { 75, 160 }, { 225, 160 });
+	//drawRectanglei_colorf({ -100, -100 }, { -200, -200 }, { 1, 0, 1 });
+	//drawRectangle2i_colorf({ 30, -30 }, { 200, -30 }, { 200, -300 }, { 30, -300 }, { 0, 1, 1 });
+
+	//point2D_i kotak[4] = {{ 100, 100 }, { 300, 100 }, { 300, 200 }, { 100, 200 }};
+	//drawPolygon(kotak, 4);
+
+	/*
+		static int go = 0;
+		static int number;
+		static int randNum[720];
+		point2D_f p[720];
+
+		for (int i = 0; i < 720; i++) {
+			p[i].x = i;
+			p[i].y = randNum[i] * sin(36 * ((i + go) / 57.3));
+		}
+
+		if (go == 0 || go % 10 == 0)
+			number = rand() % 150 + 1;
+
+		drawPolyline(p, 720);
+		go++;
+
+		for (int i = 0; i < 720; i++) {
+			if (i == 719)
+				randNum[i] = number;
+			else
+				randNum[i] = randNum[i + 1];
+		}
+
+		if (go == 720)
+			go = 0;
 	*/
 
-	drawCircleAtPoint(sunOrbit[earth], 20, n, { 0, 0.75, 0 });
-	glColor3f(0, 0, 0);
-	drawCircleAtPoint(sunOrbit[earth], 20, n);
-	drawCircleAtPoint(earthOrbit, sunOrbit[earth], 45, n);
-	drawCircleAtPoint(earthOrbit[moon], 10, n, { 1, 0.5, 0 });
-	glColor3f(0, 0, 0);
-	drawCircleAtPoint(earthOrbit[moon], 10, n);
+	/*
+		object3D_f obyek3D = {
+			8,
+			{{ 150, 150, 0 }, { 0, 150, 150 }, { -150, 150, 0 }, { 0, 150, -150 }, {150, 0, 0}, {0, 0, 150}, {-150, 0, 0}, {0, 0, -150}},
+			6,
+			{
+				{ 4, { 7, 3, 0, 4 } },
+				{ 4, { 2, 3, 7, 6 } },
+				{ 4, { 5, 6, 7, 4 } }, 
+				{ 4, { 0, 3, 2, 1 } }, 
+				{ 4, { 0, 1, 5, 4 } }, 
+				{ 4, { 1, 2, 6, 5 } }
+			}
+		};
+	*/
 
-	earth++;
-	moon += 3;
+		object3D_f obyek3D = {
+			8,
+			{ { 150, 150, 0 },{ 0, 150, 150 },{ -150, 150, 0 },{ 0, 150, -150 },{ 150, 0, 0 },{ 0, 0, 150 },{ -150, 0, 0 },{ 0, 0, -150 } },
+			12,
+			{ 
+				{ 3, { 5, 4, 0 } }, { 3, { 5, 0, 1 } }, { 3, { 5, 1, 2 } }, { 3, { 5, 2, 6 } },
+				{ 3, { 0, 3, 1 } }, { 3, { 1, 3, 2 } }, { 3, { 5, 7, 4 } }, { 3, { 6, 7, 5 } },
+				{ 3, { 4, 7, 0 } }, { 3, { 7, 3, 0 } }, { 3, { 6, 2, 7 } }, { 3, { 7, 2, 3 } }
+			}
+		};
 
-	if (earth == 360)
-		earth = 0;
+	colorRGB_f color3D[] = {
+		{ 0.25, 0, 0 }, { 0.5, 0, 0 }, { 0.75, 0, 0 }, { 1, 0, 0 }, // Merah
+		{ 0, 0.25, 0 }, { 0, 0.5, 0 }, { 0, 0.75, 0 }, { 0, 1, 0 }, // Hijau
+		{ 0, 0, 0.25 }, { 0, 0, 0.5 }, { 0, 0, 0.75 }, { 0, 0, 1 }, // Biru
+	};
 
-	if (moon == 360)
-		moon = 0;
+	static float theta = 0;
+	matrix2D_f tilting = matrixXmatrix(rotationMatrixX(theta), rotationMatrixY(theta));
+	tilting = matrixXmatrix(tilting, rotationMatrixZ(theta));
+	vector3D_f vector[100];
+	point2D_f point2D[100];
+
+	for (int i = 0; i < obyek3D.numberOfVertices; i++) { 
+		vector2D_f vectorBuff;
+
+		vector[i] = point3D_f_to_vector3D_f(obyek3D.point[i]); 
+		vectorBuff = matrixXvector(tilting, { vector[i].v[0], vector[i].v[1], vector[i].v[2] });
+
+		vector[i].v[0] = vectorBuff.v[0];
+		vector[i].v[1] = vectorBuff.v[1];
+		vector[i].v[2] = vectorBuff.v[2];
+	}
+	
+	for (int i = 0; i < obyek3D.numberOfFaces; i++) {
+		vector3D_f vectorBuffer[100];
+		int j;
+		
+		for (j = 0; j < obyek3D.face[i].numberOfVertices; j++)  
+			vectorBuffer[j] = vector[obyek3D.face[i].point[j]]; 
+		
+		vector3D_f normalVector = vectorCrossVector3D(vectorMinVector3D(vectorBuffer[1], vectorBuffer[0]), vectorMinVector3D(vectorBuffer[2], vectorBuffer[0]));
+		float normalzi = normalVector.v[2]; // Merupakan sudut pandang pengguna, indeks 0 melihat dari bawah
+
+		if (normalzi > 0) {
+			for (j = 0; j < obyek3D.face[i].numberOfVertices; j++)
+				point2D[j] = vector3D_f_to_point2D_f(vector[obyek3D.face[i].point[j]]);
+
+			//drawPolygon(point2D, obyek3D.face[i].numberOfVertices);
+			drawFillPolygon(point2D, obyek3D.face[i].numberOfVertices, color3D[i]); 
+		}
+	}
+
+	theta++;
 
 	glFlush();
 }
 
 void timer(int value) {
 	glutPostRedisplay();
-	glutTimerFunc(50, timer, 0);
+	glutTimerFunc(10, timer, 0);
 }
 
 /*
@@ -1773,12 +2313,12 @@ void timer(int value) {
 void initialize() {
 	glClearColor(1, 1, 1, 1); // Clear window content and replace with some color
 	gluOrtho2D(-360, 360, -360, 360); // Create 2D system coordinate
+	//gluOrtho2D(0, 720, -360, 360);
 }
 
 int main(int iArgc, char** cppArgv) {
 	glutInit(&iArgc, cppArgv); // Initialize glut library
-	glutInitWindowSize(720
-		, 720); // Initialize application window size
+	glutInitWindowSize(720, 720); // Initialize application window size
 	glutInitWindowPosition(100, 100); // Initialize application window location (0, 0) is define at up-left
 	glutCreateWindow("2110151023 - Faza Zulfika Permana Putra"); // Create initialized application window with title
 
